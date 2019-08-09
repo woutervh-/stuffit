135c9dd910f1e93f18cf3678c03c6bfbfd7756d2
Wouter van Heeswijk <woutervh@gmail.com>
2019-08-09 11:07:46 +0200

    Added consumers

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ export { Sink } from './sink';
 export { Store } from './store';
 export { Subscription } from './subscription';
 
+import * as Consumers from './consumers';
 import * as Operators from './operators';
 import * as Producers from './producers';
 
 export {
+    Consumers,
     Operators,
     Producers
 };
