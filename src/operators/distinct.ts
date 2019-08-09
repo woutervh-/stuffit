2a79d42b4149cea6877349a3b3cc4b9027dd4660
Wouter van Heeswijk <woutervh@gmail.com>
2019-08-09 11:56:11 +0200

    Added distinct operator

---
+++ src/operators/distinct.ts
@@ -0,0 +1,44 @@
+import { Store } from '../store';
+import { Subscription } from '../subscription';
+
+export class DistinctStore<T> extends Store<T> {
+    private source: Store<T>;
+    private testEquality: (previous: T, next: T) => boolean;
+    private subscription: Subscription | undefined = undefined;
+    private currentState: T;
+
+    constructor(source: Store<T>, testEquality: (previous: T, next: T) => boolean) {
+        super();
+        this.source = source;
+        this.testEquality = testEquality;
+        this.currentState = source.state;
+    }
+
+    public get state() {
+        return this.currentState;
+    }
+
+    protected start() {
+        if (this.subscription === undefined) {
+            this.subscription = this.source.subscribe(this.handleNext);
+        }
+    }
+
+    protected stop() {
+        if (this.subscription !== undefined) {
+            this.subscription.unsubscribe();
+            this.subscription = undefined;
+        }
+    }
+
+    private handleNext = (value: T) => {
+        if (!this.testEquality(this.currentState, value)) {
+            this.currentState = value;
+            this.notify(value);
+        }
+    }
+}
+
+export const distinct = <T>(testEquality: (previous: T, next: T) => boolean) => (source: Store<T>): DistinctStore<T> => {
+    return new DistinctStore(source, testEquality);
+};
--- src/operators/index.ts
+++ src/operators/index.ts
@@ -1,4 +1,5 @@
 export { combine, combineApply } from './combine';
+export { distinct } from './distinct';
 export { flatten } from './flatten';
 export { history } from './history';
 export { map } from './map';
