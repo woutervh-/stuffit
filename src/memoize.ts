af8625ea3e3716281b8a27080f2833541946cab5
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-10-28 15:24:05 +0100

    Added memoize function

---
+++ src/memoize.ts
@@ -0,0 +1,25 @@
+const equals = (left: unknown[], right: unknown[]) => {
+    if (left.length !== right.length) {
+        return false;
+    }
+    for (let i = 0; i < left.length; i++) {
+        if (left[i] !== right[i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+export const memoize = <T extends (...args: unknown[]) => unknown>(fn: T): T => {
+    let lastValues: { args: unknown[], returnValue: unknown } | null = null;
+
+    return function (this: unknown, ...args: unknown[]) {
+        if (lastValues && equals(args, lastValues.args)) {
+            return lastValues.returnValue;
+        } else {
+            const returnValue = fn.apply(this, args);
+            lastValues = { args, returnValue };
+            return returnValue;
+        }
+    } as T;
+};
