65aae7d7d2627a9a28e16172c070c5f929220941
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-10-30 12:45:32 +0100

    Add throttle unit test

---
+++ test/operators/throttle.test.ts
@@ -0,0 +1,51 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import { ThrottleStore } from '../../src/operators/throttle';
+import { PushStore } from '../../src/push-store';
+
+let clock: sinon.SinonFakeTimers;
+let source: PushStore<number>;
+
+beforeEach(() => {
+    clock = sinon.useFakeTimers();
+    source = new PushStore(0);
+});
+
+afterEach(() => {
+    clock.restore();
+});
+
+describe('ThrottleStore', () => {
+    describe('#subscribe', () => {
+        it('Will notify subscribers of state changes at most once per configured timeout.', () => {
+            let count = 0;
+            const store = new ThrottleStore(source, 100);
+            const subscription = store.subscribe(() => count += 1);
+
+            source.setState(1);
+            chai.assert.strictEqual(count, 1);
+            source.setState(2);
+            chai.assert.strictEqual(count, 1);
+            source.setState(3);
+            chai.assert.strictEqual(count, 1);
+
+            clock.tick(150);
+
+            chai.assert.strictEqual(count, 2);
+            source.setState(4);
+            chai.assert.strictEqual(count, 2);
+
+            clock.tick(150);
+
+            chai.assert.strictEqual(count, 3);
+
+            clock.tick(150);
+
+            chai.assert.strictEqual(count, 3);
+            source.setState(5);
+            chai.assert.strictEqual(count, 4);
+
+            subscription.unsubscribe();
+        });
+    });
+});
